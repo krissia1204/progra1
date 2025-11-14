f2eb1d1959df9c9ce19901be0e3b5a736d1e0b1e
krissia1204 <issi122404@gmail.com>
2025-11-14 12:52:22 -0600

    Agregué el main

--- AlgoritmosBusqueda/Algoritmos.java
+++ AlgoritmosBusqueda/Algoritmos.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Algoritmos{
 
     public int linearSearch(int arr[], int num){
@@ -12,6 +13,8 @@ public class Algoritmos{
     }
 
     public int binarySearch(int arr[],int low, int high, int num){
+       
+       
         if(high>=low){
             int mid= low + (high - low)/2;
 
@@ -22,9 +25,10 @@ public class Algoritmos{
             if(arr[mid]>num){
                 return binarySearch(arr, low, mid-1, num);
             }
-   
+              return binarySearch(arr,mid+1, high, num);
         }
-        return binarySearch(arr, mid+1, high, num);
+        return -1;
+      
     }
 
     public void bubbleSort(int arr[]){
@@ -81,11 +85,100 @@ public class Algoritmos{
 
     public static void main(String[] args) {
         Algoritmos agrt= new Algoritmos();
+        Scanner sc= new Scanner(System.in);
         int arr[]= {12,10,70,35,74,32,1,0};
+        System.out.println("Bienvenido, ¿Qué desea acción realizar? "
+                + "\n1. Probar algoritmo de busqueda lineal"
+                + "\n2. Probar algorito de busqueda binaria"
+                + "\n3. Probar algoritmo de selección"
+                + "\n4. Probar algoritmo de burbuja"
+                + "\n5. Probar algoritmo de inserción"
+                + "\n6. Salir");
 
-        
+        int opcion= sc.nextInt();
 
-    }
+        if (opcion<1|| opcion>5){
 
-}
+            System.out.println("Digite una opción valida");
+        }
 
+        else {
+
+            while (opcion!=6){
+
+                switch (opcion){
+
+                    case 1:
+                        System.out.println("Digite el número que desea buscar dentro del arreglo: ");
+                        int num= sc.nextInt();
+                        int result= agrt.linearSearch(arr, num);
+                        if (result == -1){
+                            System.out.println("El número no se encuentra en el arreglo");
+                        }
+                        else {
+                            System.out.println("El número se encuentra en el indice: " + result);
+                        }
+
+                        break;
+                        
+                        
+
+
+                    case 2:
+                       System.out.println("Digite el número que desea buscar dentro del arreglo: ");
+                        num= sc.nextInt();
+                        int high= arr.length -1;
+                        agrt.bubbleSort(arr);
+                        result= agrt.binarySearch(arr, 0, high, num);
+                        if (result == -1){
+                            System.out.println("El número no se encuentra en el arreglo");
+                        }
+                        else {
+                            System.out.println("El número se encuentra en el indice: " + result);
+                            
+                        }
+
+                        break;
+                    case 3:
+                        System.out.println("Arreglo original: ");
+                        printArray(arr);
+                        agrt.selectionSort(arr);
+                        System.out.println("Arreglo ordenado: ");
+                        printArray(arr);
+                        break;
+
+                    case 4:
+                        System.out.println("Arreglo original: "); 
+                        printArray(arr);
+                        agrt.bubbleSort(arr);
+                        System.out.println("Arreglo ordenado: ");
+                        printArray(arr);
+
+                        break;
+                    case 5:
+                        System.out.println("Arreglo original: ");
+                        printArray(arr);
+                        agrt.insertionSort(arr);
+                        System.out.println("Arreglo ordenado: ");
+                        printArray(arr);
+
+                    }
+                System.out.println("¿Qué desea acción realizar? "
+                        + "\n1. Probar algoritmo de busqueda lineal"
+                        + "\n2. Probar algorito de busqueda binaria"
+                        + "\n3. Probar algoritmo de selección"
+                        + "\n4. Probar algoritmo de burbuja"
+                        + "\n5. Probar algoritmo de inserción"
+                        + "\n6. Salir");
+                    opcion= sc.nextInt();
+                }
+                System.out.println("Gracias por usar el programa. Hasta luego!");
+
+
+                    
+                }}
+
+                    
+                    
+                        
+            }
